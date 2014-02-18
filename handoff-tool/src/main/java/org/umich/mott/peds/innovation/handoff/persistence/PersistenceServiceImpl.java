e5cce048d83b31b1743ddf33cadefae2d0cf8084
Kyle <kdkavanagh@gmail.com>
2014-02-18 10:10:12 -0500

    change color fergodsakes

--- handoff-tool/src/main/java/org/umich/mott/peds/innovation/handoff/persistence/PersistenceServiceImpl.java
+++ handoff-tool/src/main/java/org/umich/mott/peds/innovation/handoff/persistence/PersistenceServiceImpl.java
@@ -3,8 +3,11 @@ package org.umich.mott.peds.innovation.handoff.persistence;
 /**
  * @author Kyle D. Kavanagh
  * @date Feb 18, 2014
- *
+ * 
  */
 public class PersistenceServiceImpl implements PersistenceService {
 
+  public PersistenceServiceImpl() {
+    // Init the db connection here
+  }
 }
