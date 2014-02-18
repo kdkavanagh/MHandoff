6f6772a61e69514d951f854fc2e658826cc96d1b
Kyle <kdkavanagh@gmail.com>
2014-02-18 09:46:14 -0500

    add RequestMethod; add persistence interface

---
+++ handoff-tool/src/main/java/org/umich/mott/peds/innovation/handoff/RequestMethod.java
@@ -0,0 +1,5 @@
+package org.umich.mott.peds.innovation.handoff;
+
+public enum RequestMethod {
+  POST, GET;
+}
--- handoff-tool/src/main/java/org/umich/mott/peds/innovation/handoff/dashboard/actions/AddPatientAction.java
+++ handoff-tool/src/main/java/org/umich/mott/peds/innovation/handoff/dashboard/actions/AddPatientAction.java
@@ -4,19 +4,20 @@
 package org.umich.mott.peds.innovation.handoff.dashboard.actions;
 
 import org.umich.mott.peds.innovation.handoff.Action;
-import org.umich.mott.peds.innovation.handoff.ActionMapping;
 import org.umich.mott.peds.innovation.handoff.ActionContext;
+import org.umich.mott.peds.innovation.handoff.ActionMapping;
+import org.umich.mott.peds.innovation.handoff.RequestMethod;
 
 /**
  * @author Kyle D. Kavanagh
  * @date Feb 17, 2014
  * 
  */
-@ActionMapping(method = "POST", path = "dashboard/addPatient.do")
+@ActionMapping(method = RequestMethod.POST, path = "dashboard/addPatient.do")
 public class AddPatientAction implements Action {
 
-	public String execute(ActionContext context) throws Exception {
-		return null;
-	}
+  public String execute(ActionContext context) throws Exception {
+    return null;
+  }
 
 }
