8d24409d3cedf2221f5d1050dde2d470ff2319a9
Kyle <kdkavanagh@gmail.com>
2014-02-20 11:48:53 -0500

    almost got adding notes to grid working

--- handoff-tool/src/main/webapp/patientView.js
+++ handoff-tool/src/main/webapp/patientView.js
@@ -1,56 +1,60 @@
 $(function() {
 
-var Patient = Backbone.Model.extend({
-	initialize: function(){
-		alert("New patient model init");
-	}
-});
+	var Note = Backbone.Model.extend({
+		url:"",
+		initialize: function(){
+			alert("New Note model init");
+		}
+	});
 
-var noteCollection = Backbone.Collection.extend({
-	model : Patient,
-	url : 'https://localhost:8443/patient/items.do?patient=kyle&type=note',
-	fetch : function() {
-		$.ajax({
-			type : 'GET',
-			url : this.url,
-			success : function(data) {
-				console.log(data);
+	var noteCollection = Backbone.Collection.extend({
+		model : Note,
+		url : '/patient/items.do?patient=kyle&type=note',
+	});
+
+	var theView = Backbone.View.extend({
+		tagName: 'li',
+		el: 'body',
+
+		events: {
+			'click #buttonPress': "render",
+			'click #buttonPress': "addItem",
+		},
+
+		initialize: function () {
+
+			console.log("Creating new view");
+			_.bindAll(this, 'render');
+			this.notes = new noteCollection();
+
+			this.notes.on('reset', this.render, this);
+			this.notes.on('change', this.render);
+		},
+
+		addItem: function() {
+			console.log("Adding item");
+			this.notes.fetch({reset:true});
+			
+		},
+
+			render: function(){
+				console.log("go for Loadme");
+				var row = 1;
+				this.notes.each(function(note, index) { // iterate through the collection
+					console.log("Adding "+note.get("noteId"));
+					col = index % 4;
+					if(col == 0) {
+						row += 1;
+					}
+					
+					$("#notesList").append("<li data-row=\""+row+"\" data-col=\""+col+"\" data-sizex=\"1\" data-sizey=\"1\">"+note.get("text")+"</li>"
+							);
+				});
+
+				return this;
 			}
 		});
-	}
-});
 
-var theView = Backbone.View.extend({
-	tagName: 'li',
-	el: 'body',
+			var view = new theView();
 
-	events: {
-		'click #buttonPress': "render",
-	},
-
-	initialize: function () {
-
-		console.log("Creating new view");
-		_.bindAll(this, 'render');
-		this.collection = new noteCollection();
-		this.collection.fetch({reset:true});
-		this.collection.on('reset', this.render, this);
-		this.render();
-		console.log("Done rendering");
-	},
-
-	render: function(){
-		console.log("go for Loadme");
-
-		this.collection.each(function(patient) { // iterate through the collection
-			console.log(patient.get("name"));
-			$("#patientList").append("<li>"+patient.get("name")+"</li>");
-		});
-
-		return this;
-	}
-});
-
-var view = new theView();
-
-});
+	});
