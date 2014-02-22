016656cae1124b2b74830c929406e4d53aa09d01
Kyle <kdkavanagh@gmail.com>
2014-02-22 15:52:21 -0500

    work on open note view

--- handoff-tool/src/main/webapp/patientView.js
+++ handoff-tool/src/main/webapp/patientView.js
@@ -1,232 +1,264 @@
 $(function() {
 
-	var Note = Backbone.Model.extend({
-		url:"",
-		defaults : {
-			noteId : "0",
-			priority :"1",
-			reporter:"N/A",
-			badgeLevel:"",
-		},
-	});
+    var Note = Backbone.Model.extend({
+        url:"",
+        defaults : {
+            noteId : "0",
+            priority :"1",
+            reporter:"N/A",
+            badgeLevel:"",
+        },
+    });
 
-	var NoteCollection = Backbone.Collection.extend({
-		model : Note,
-		url : '/patient/items.do?type=note',
-	});
+    var NoteCollection = Backbone.Collection.extend({
+        model : Note,
+        url : '/patient/items.do?type=note',
+    });
 
-	var IndividualNoteView = Backbone.View.extend({
-		tagName: 'li',
-		template:$("#indivNoteTemplate").html(),
+    var NoteModalView = Backbone.View.extend({
 
-		events: {
-			'click span#closeIcon': "buttonClickHandler",
-		},
+        template:$("#modalNoteTemplate").html(),
+        
+        initialize : function (options) {
+            console.log("Creating new modal note view");
+            this.options = options || {};
+            this.noteModel = this.options.noteModel;
+            this.noteModel.on('change', this.render, this);
+            return this;
+        },
+        
+        render:function() {
+            var tmpl = _.template(this.template); //tmpl is a function that takes a JSON and returns html
+            console.log(tmpl(this.noteModel.toJSON()));
+            this.setElement(tmpl(this.noteModel.toJSON()));
+            this.$el.modal('show');
+        }
 
-		initialize : function (options) {
-			console.log("Creating new indiv note view");
-			this.options = options || {};
-			this.gridster = this.options.gridster;
-			this.noteModel = this.options.noteModel;
-			this.row = this.options.row;
-			this.col = this.options.col;
-			console.log(this.noteModel.get("priority"));
-			if(this.noteModel.get("priority") == 1) {
-				console.log("setting");
-				this.noteModel.set("badgeLevel", "badge-error");
-			}
-			this.noteModel.on('change', this.render, this);
-			return this;
-		},
+    });
 
-		render: function(){
-			
-			var tmpl = _.template(this.template); //tmpl is a function that takes a JSON and returns html
-			this.setElement(tmpl(this.noteModel.toJSON()));
-			this.check();
-			this.gridster.add_widget(this.el);
-		},
+    var IndividualNoteView = Backbone.View.extend({
+        tagName: 'li',
+        template:$("#indivNoteTemplate").html(),
 
-		check: function() {
-			var checker = new checkLength();
-			checker.check();
-			//maybe set the vertical height of the gridster obj here??
-			//var moreThis = this.$el.find('.more');
-			//moreThis.popover({content:this.noteModel.get("text")});
-		},
+        events: {
+            'click span#closeIcon': "buttonClickHandler",
+            'click button#openNoteButton' : "openNote",
+        },
 
-		buttonClickHandler : function(event){
-			console.log("Removing...");
-			//this.noteModel.destroy();
-			this.gridster.remove_widget(this.$el);
-			this.remove();
+        initialize : function (options) {
+            console.log("Creating new indiv note view");
+            this.options = options || {};
+            this.gridster = this.options.gridster;
+            this.noteModel = this.options.noteModel;
+            this.row = this.options.row;
+            this.col = this.options.col;
+            if(this.noteModel.get("priority") == 1) {
+                console.log("setting");
+                this.noteModel.set("badgeLevel", "badge-error");
+            }
+            this.noteModel.on('change', this.render, this);
+            return this;
+        },
 
-			return false;
-		},
+        render: function(){
 
-		remove: function() {
-			Backbone.View.prototype.remove.apply(this, arguments);
-			this.trigger('remove', this);
-			return this;
-		},
+            var tmpl = _.template(this.template); //tmpl is a function that takes a JSON and returns html
+            this.setElement(tmpl(this.noteModel.toJSON()));
+            this.check();
+            this.gridster.add_widget(this.el);
+        },
 
-		destroy_full: function(event) {
-			console.log("Destroying item");
-			this.unbind(); // Unbind all local event bindings
-			this.noteModel.unbind( 'change', this.render, this ); // Unbind reference to the model
-			this.noteModel.destroy();
-			this.options.parent.unbind( 'close:all', this.close, this ); // Unbind reference to the parent view
+        check: function() {
+            var checker = new checkLength();
+            checker.check();
+            //maybe set the vertical height of the gridster obj here??
+            //var moreThis = this.$el.find('.more');
+            //moreThis.popover({content:this.noteModel.get("text")});
+        },
+        
+        openNote: function() {
+            console.log("opening note");
+            var modal = new NoteModalView({el:$("modalContainer"), noteModel:this.noteModel});
+            modal.render();
+        },
 
-			this.remove(); // Remove view from DOM
+        buttonClickHandler : function(event){
+            console.log("Removing...");
+            //this.noteModel.destroy();
+            this.gridster.remove_widget(this.$el);
+            this.remove();
 
-			delete this.$el; // Delete the jQuery wrapped object variable
-			delete this.el; // Delete the variable reference to this node
+            return false;
+        },
 
-		}
-	});
+        remove: function() {
+            Backbone.View.prototype.remove.apply(this, arguments);
+            this.trigger('remove', this);
+            return this;
+        },
 
-	var NoteGridView = Backbone.View.extend({
+        destroy_full: function(event) {
+            console.log("Destroying item");
+            this.unbind(); // Unbind all local event bindings
+            this.noteModel.unbind( 'change', this.render, this ); // Unbind reference to the model
+            this.noteModel.destroy();
+            this.options.parent.unbind( 'close:all', this.close, this ); // Unbind reference to the parent view
 
-		el: '.base',
-		mostRecentlyDeletedView : null,
+            this.remove(); // Remove view from DOM
 
-		events: {
-			'click #buttonPress': "getItems",
-			'click #addDummyButton': "addItem",
-			'click #undoButton' : "undoRemove",
-		},
+            delete this.$el; // Delete the jQuery wrapped object variable
+            delete this.el; // Delete the variable reference to this node
 
-		initialize: function () {
+        }
+    });
 
-			console.log("Creating new view");
-			_.bindAll(this, 'render');
+    var NoteGridView = Backbone.View.extend({
 
-			this.notes = new NoteCollection();
-			this.notes.fetch({ data: $.param({ patient: "kyle",}) 
-				,reset:true,});
-			this.notes.on('reset', this.generateViews, this);
-			//this.notes.on('change', this.render);
-			this.notes.on('add', this.newItemAdded, this);
-			this.noteViews = new Array();
-			this.garbageViews = new Array();
-		},
+        el: '.base',
+        mostRecentlyDeletedView : null,
 
-		createView: function(note, row, col, self) {
-			var gridsterObj = $("#noteGrid ul").gridster().data('gridster');
-			var noteView = new IndividualNoteView({parent : self, noteModel:note, row:row, col:col, gridster : gridsterObj});
-			self.noteViews.push(noteView);
-			noteView.on('remove', self.noteRemoved, self);
-			return noteView;
+        events: {
+            'click #buttonPress': "getItems",
+            'click #addDummyButton': "addItem",
+            'click #undoButton' : "undoRemove",
+        },
 
-		},
+        initialize: function () {
 
-		generateViews: function() {
-			//Destroy existing views
-			while (this.noteViews.length > 0) {
-				this.garbageViews.push(this.noteViews.pop());
-			}
+            console.log("Creating new view");
+            _.bindAll(this, 'render');
 
-			var row = 0;
-			var self = this;
-			this.notes.each(function(note, index) { 
-				col = index % 4;
-				if(col == 0) {
-					row += 1;
-				}
-				self.createView(note, row, col, self);
-			});
-			this.render();
-		},
+            this.notes = new NoteCollection();
+            this.notes.fetch({ data: $.param({ patient: "kyle",}) 
+                ,reset:true,});
+            this.notes.on('reset', this.generateViews, this);
+            //this.notes.on('change', this.render);
+            this.notes.on('add', this.newItemAdded, this);
+            this.noteViews = new Array();
+            this.garbageViews = new Array();
+        },
 
-		getItems: function() {
-			console.log("Getting all items");
-			this.notes.fetch({ data: $.param({ patient: "kyle",}) 
-				,reset:true,});
+        createView: function(note, row, col, self) {
+            var gridsterObj = $("#noteGrid ul").gridster().data('gridster');
+            var noteView = new IndividualNoteView({parent : self, noteModel:note, row:row, col:col, gridster : gridsterObj});
+            self.noteViews.push(noteView);
+            noteView.on('remove', self.noteRemoved, self);
+            return noteView;
 
-		},
-		addItem: function() {
-			console.log("Adding item");
-			//create the note
-			var note = new Note({text:"My dummy note",});
-			this.notes.add(note);
-		},
-		
-		newItemAdded:function(note) {
-			this.createView(note, 0, 0, this).render();
-		},
+        },
 
+        generateViews: function() {
+            //Destroy existing views
+            while (this.noteViews.length > 0) {
+                this.garbageViews.push(this.noteViews.pop());
+            }
 
-		undoRemove:function() {
-			if(this.mostRecentlyDeletedView != null) {
-				this.noteViews.push(this.mostRecentlyDeletedView);
-				this.mostRecentlyDeletedView.render();
-				this.mostRecentlyDeletedView = null;
-			}
-		},
+            var row = 0;
+            var self = this;
+            this.notes.each(function(note, index) { 
+                col = index % 4;
+                if(col == 0) {
+                    row += 1;
+                }
+                self.createView(note, row, col, self);
+            });
+            this.render();
+        },
 
-		noteRemoved:function(event) {
-			if(this.mostRecentlyDeletedView != null) {
-				//There was a previously deleted note, lets get rid of it for good
-				console.log("Permanently deleting note");
-				this.mostRecentlyDeletedView.destroy_full(null);
-			}
-			//remove the view from our list of views to render
-			var index = this.noteViews.indexOf(event);
-			if (index > -1) {
-				this.noteViews.splice(index, 1);
-			}
-			this.mostRecentlyDeletedView = event;
-		},
+        getItems: function() {
+            console.log("Getting all items");
+            this.notes.fetch({ data: $.param({ patient: "kyle",}) 
+                ,reset:true,});
 
-		render: function(){
-			var gridsterObj = $("#noteGrid ul").gridster().data('gridster');
-			gridsterObj.remove_all_widgets();
+        },
+        addItem: function() {
+            console.log("Adding item");
+            //create the note
+            var note = new Note({text:"My dummy note",});
+            this.notes.add(note);
+        },
 
-			for (var i = 0; i < this.noteViews.length; i++) {
-				this.noteViews[i].render();
-			}
-
-			//delete any garbage we have (must be done after remove_all_widgets()
-			while(this.garbageViews.length > 0) {
-				this.garbageViews.pop().destroy_full(null);
-			}
-
-			return this;
-		}
-	});
+        newItemAdded:function(note) {
+            this.createView(note, 0, 0, this).render();
+        },
 
 
-	function checkLength() {
-		this.showing = new Array();
-	}
-
-	checkLength.prototype.check = function() {
-		//Check to see if we need the "more" text
-		var that = this;
-		$('.noteTextArea').each(function (index) {
-			var article = $(this);
-			var theP = article.find('p');
-			var theMore = article.find('.more');
-			//Attach a popover to theMore
-			 if(theP[0].scrollHeight >= $(this).height()) {
-				theMore.show();
-				that.showing[index] = true;
-			} else {
-				if (!article.hasClass('active')) {
-					theMore.hide();
-					
-					that.showing[index] = false;
-				} else {
-					that.showing[index] = false;
-					
-				}
-			}
-			theMore.text(that.showing[index] ? "More..." : "Less...");
-		});
-	};
+        undoRemove:function() {
+            if(this.mostRecentlyDeletedView != null) {
+                $('#undoAlert').alert('close'); 
+                this.noteViews.push(this.mostRecentlyDeletedView);
+                this.mostRecentlyDeletedView.render();
+                this.mostRecentlyDeletedView = null;
+            }
+        },
 
 
 
-	var view = new NoteGridView();
+        noteRemoved:function(event) {
+            if(this.mostRecentlyDeletedView != null) {
+                //There was a previously deleted note, lets get rid of it for good
+                console.log("Permanently deleting note");
+                this.mostRecentlyDeletedView.destroy_full(null);
+            }
+            //remove the view from our list of views to render
+            var index = this.noteViews.indexOf(event);
+            if (index > -1) {
+                this.noteViews.splice(index, 1);
+            }
+            this.mostRecentlyDeletedView = event;
+            $('#undoAlertHolder').html('<div id="undoAlert" class="alert alert-info alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><span><button class="btn btn-default" id="undoButton">Undo..</button></span></div>');
+
+        },
+
+        render: function(){
+            var gridsterObj = $("#noteGrid ul").gridster().data('gridster');
+            gridsterObj.remove_all_widgets();
+
+            for (var i = 0; i < this.noteViews.length; i++) {
+                this.noteViews[i].render();
+            }
+
+            //delete any garbage we have (must be done after remove_all_widgets()
+            while(this.garbageViews.length > 0) {
+                this.garbageViews.pop().destroy_full(null);
+            }
+
+            return this;
+        }
+    });
+
+
+    function checkLength() {
+        this.showing = new Array();
+    }
+
+    checkLength.prototype.check = function() {
+        //Check to see if we need the "more" text
+        var that = this;
+        $('.noteTextArea').each(function (index) {
+            var article = $(this);
+            var theP = article.find('p');
+            var theMore = article.find('.more');
+            //Attach a popover to theMore
+            if(theP[0].scrollHeight >= $(this).height()) {
+                theMore.show();
+                that.showing[index] = true;
+            } else {
+                if (!article.hasClass('active')) {
+                    theMore.hide();
+
+                    that.showing[index] = false;
+                } else {
+                    that.showing[index] = false;
+
+                }
+            }
+            theMore.text(that.showing[index] ? "More..." : "Less...");
+        });
+    };
+
+
+
+    var view = new NoteGridView();
 
 });
